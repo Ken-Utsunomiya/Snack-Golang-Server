0c94332d66aebaab4c76dfea7661ba7636850f3e
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-07-21 16:12:23 -0700

    fix: fix payment model for gorm

--- src/models/payment.go
+++ src/models/payment.go
@@ -6,7 +6,6 @@ import (
 
 type Payment struct {
 	PaymentId int `json:"paymentId" gorm:"primaryKey;autoIncrement;not null"`
-	UserId int `json:"userId" gorm:"foreignKey:UserId;not null"`
 	PaymentAmount int `json:"paymentAmount" gorm:"not null"`
 	PaymentDTM time.Time `json:"paymentDTM" gorm:"not null"`
 	CreatedBy string `json:"CreatedBy" gorm:"not null"`
