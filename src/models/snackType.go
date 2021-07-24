5a285432403f04bb11942646191c031685efc7b5
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-07-23 17:21:45 -0700

    fix: update snack_types model

--- src/models/snackType.go
+++ src/models/snackType.go
@@ -1,6 +1,10 @@
 package models
 
 type SnackType struct {
-	SnackTypeId int `json:"snackTypeId"`
-	SnackTypeName string `json:"snackTypeName"`
+	ID 	 uint 	`gorm:"column:snack_type_id;not null"`
+	Name string `gorm:"column:snack_type_name;not null;size:128"`
+}
+
+func (SnackType) TableName() string {
+	return "snack_types"
 }
