61351cbbf1ebf3f47328256b6cc78e4b5b1c2641
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-07-23 17:45:14 -0700

    fix: update user model

--- src/models/user.go
+++ src/models/user.go
@@ -1,12 +1,19 @@
 package models
 
+import "time"
+
 type User struct {
-	Id int `json:"userId"`
-	Email string `json:"email"`
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastName"`
-	ImageURI string `json:"imageURI"`
-	Balance int `json:"balance"`
-	IsAdmin bool `json:"isAdmin"`
-	IsActive bool `json:"isActive"`
+	ID 				uint 	 		`gorm:"column:user_id;primaryKey;not null"`
+	Email 		string 		`gorm:"column:email_address;unique;not null;size:128"`
+	FirstName string 		`gorm:"column:first_name;not null;size:128"`
+	LastName 	string 		`gorm:"column:last_name;not null;size:128"`
+	ImageURI 	string 		`gorm:"column:image_uri;not null"`
+	Balance 	int 	 		`gorm:"column:balance;not null"`
+	IsAdmin 	bool 	 		`gorm:"column:is_admin;not null"`
+	IsActive 	bool 	 		`gorm:"column:is_active;not null"`
+	DeletedAt time.Time `gorm:"column:deleted_at"`
+}
+
+func (User) TableName() string {
+	return "users"
 }
