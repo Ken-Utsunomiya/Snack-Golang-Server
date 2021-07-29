47b497c2f30670805b3db09ac2e0c312f70e648a
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-07-29 15:45:20 -0700

    fix: add foreign key to snackbatch model

--- src/models/snackbatch.go
+++ src/models/snackbatch.go
@@ -6,8 +6,10 @@ import (
 
 type SnackBatch struct {
 	ID 						uint 			`gorm:"column:snack_batch_id;primaryKey;not null"`
+	SnackID       uint      `gorm:"column:snack_id;not null"`
 	Quantity 			uint 			`gorm:"column:quantity;not null"`
 	ExpirationDTM time.Time `gorm:"column:expiration_dtm"`
+	Snack         Snack
 }
 
 func (SnackBatch) TableName() string {
