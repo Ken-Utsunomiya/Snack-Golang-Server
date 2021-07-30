747e5b0829d6bc6a480f6e4095e1392dbccb695d
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-07-30 03:25:36 -0700

    feat: add suggestion service

--- src/services/suggestion.go
+++ src/services/suggestion.go
@@ -1,1 +1,17 @@
 package services
+
+import "Snack-Golang-Server/src/models"
+
+type SuggestionService struct {}
+
+func (SuggestionService) GetSuggestionList() []models.Suggestion {
+	return nil
+}
+
+func (SuggestionService) AddSuggestion(suggestion *models.Suggestion) error {
+	return nil
+}
+
+func (SuggestionService) DeleteSuggestion(id uint) error {
+	return nil
+}
