c8e578016ee2fe529be17a7c06245745cbf9db65
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-07-30 03:15:30 -0700

    feat: add payment service

--- src/services/payment.go
+++ src/services/payment.go
@@ -1,1 +1,17 @@
 package services
+
+import "Snack-Golang-Server/src/models"
+
+type PaymentService struct {}
+
+func (PaymentService) GetUserPaymentList() []models.Snack {
+	return nil
+}
+
+func (PaymentService) AddPayment(payment *models.Payment) error {
+	return nil
+}
+
+func (PaymentService) AddPaymentAll(payments []models.Payment) error {
+	return nil
+}
