d40994a8a2349f58f210d4a270ea22417db2be0e
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-07-30 03:34:52 -0700

    feat: add transaction service

--- src/services/transaction.go
+++ src/services/transaction.go
@@ -1,1 +1,29 @@
 package services
+
+import "Snack-Golang-Server/src/models"
+
+type TransactionService struct {}
+
+func (TransactionService) GetUserTransactionList() []models.Transaction {
+	return nil
+}
+
+func (TransactionService) GetUserTransaction(userId uint) *models.Transaction {
+	return nil
+}
+
+func (TransactionService) AddTransaction(transaction *models.Transaction) error {
+	return nil
+}
+
+func (TransactionService) UpdateTransaction(transaction *models.Transaction) error {
+	return nil
+}
+
+func (TransactionService) GetPendingOrderList(userId uint) []models.Transaction {
+	return nil
+}
+
+func (TransactionService) GetPopularSnackList() []models.Snack {
+	return nil
+}
