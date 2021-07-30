b26d1902d7a2f50be5e5f69f82a788eeb7d73867
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-07-30 03:18:37 -0700

    feat: add snackbatch service

--- src/services/snackbatch.go
+++ src/services/snackbatch.go
@@ -1,1 +1,21 @@
 package services
+
+import "Snack-Golang-Server/src/models"
+
+type SnackBatchService struct {}
+
+func (SnackBatchService) GetSnackBatchList() []models.SnackBatch {
+	return nil
+}
+
+func (SnackBatchService) AddSnackBatch(snackBatch *models.SnackBatch) error {
+	return nil
+}
+
+func (SnackBatchService) UpdateSnackBatch(snack *models.SnackBatch) error {
+	return nil
+}
+
+func (SnackBatchService) DeleteSnackBatch(id uint) error {
+	return nil
+}
