c685a808a5cffc6ad91a17a73e1e040ae59d5278
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-01 01:46:31 -0700

    fix: update payment model

--- src/models/snack.go
+++ src/models/snack.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Snack struct {
-	ID 			       uint 		 `json:"" gorm:"column:snack_id;primaryKey;autoIncrement"`
-	SnackTypeID    uint      `json:"" gorm:"column:snack_type_id;not null"`
+	ID 			       int 		 `json:"" gorm:"column:snack_id;primaryKey;autoIncrement"`
+	SnackTypeID    int      `json:"" gorm:"column:snack_type_id;not null"`
 	Name           string 	 `gorm:"column:snack_name;unique;not null;size:128"`
 	Description    string    `gorm:"column:description;not null;size:128"`
 	ImageURI       string    `gorm:"column:image_uri;not null"`
-	Price          uint 		 `gorm:"column:price;not null"`
+	Price          int 		 `gorm:"column:price;not null"`
 	IsActive       bool 	   `gorm:"column:is_active;not null"`
-	OrderThreshold uint 		 `gorm:"column:order_threshold"`
+	OrderThreshold int 		 `gorm:"column:order_threshold"`
 	LastUpdatedDTM time.Time `gorm:"column:last_updated_dtm;not null"`
 	LastUpdatedBy  string    `gorm:"column:last_updated_by;not null;size:128"`
 	SnackType      SnackType
