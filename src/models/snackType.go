e2074494ee4288f2dc15b7c35a180ff069d3902c
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-01 02:02:06 -0700

    fix: update snacktype model

--- src/models/snackType.go
+++ src/models/snackType.go
@@ -1,8 +1,8 @@
 package models
 
 type SnackType struct {
-	ID 	 uint 	`gorm:"column:snack_type_id;primaryKey;not null"`
-	Name string `gorm:"column:snack_type_name;unique;not null;size:128"`
+	ID 	 int 	  `json:"snack_type_id"   gorm:"column:snack_type_id;primaryKey;not null"`
+	Name string `json:"snack_type_name" gorm:"column:snack_type_name;unique;not null;size:128"`
 }
 
 func (SnackType) TableName() string {
