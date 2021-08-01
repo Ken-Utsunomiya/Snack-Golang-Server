f601d935c02ef5e45fdb68ee0b650b14d7e19df7
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-01 01:45:16 -0700

    fix: update payment model

--- src/models/payment.go
+++ src/models/payment.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Payment struct {
-	ID 						uint 			`gorm:"column:payment_id;primaryKey;autoIncrement;not null"`
-	UserID        uint      `gorm:"column:user_id;not null"`
-	PaymentAmount uint 			`gorm:"column:payment_amount;not null"`
-	PaymentDTM 		time.Time `gorm:"column:payment_dtm;not null"`
-	CreatedBy 		string 		`gorm:"column:created_by;not null;size:128"`
+	ID 						uint 			`json:"payment_id" gorm:"column:payment_id;primaryKey;autoIncrement;not null"`
+	UserID        uint      `json:"user_id" gorm:"column:user_id;not null"`
+	PaymentAmount uint 			`json:"payment_amount" gorm:"column:payment_amount;not null"`
+	PaymentDTM 		time.Time `json:"payment_dtm" gorm:"column:payment_dtm;not null"`
+	CreatedBy 		string 		`json:"created_by" gorm:"column:created_by;not null;size:128"`
 	User          User
 }
 
