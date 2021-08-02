58ea554d31874314c65f1a413557913ad77312c2
Ken-Utsunomiya <81115999+Ken-Utsunomiya@users.noreply.github.com>
2021-08-02 16:23:40 -0700

    Update suggestion.go

--- src/models/suggestion.go
+++ src/models/suggestion.go
@@ -5,10 +5,10 @@ import (
 )
 
 type Suggestion struct {
-	ID 						 int 		   `json:"suggestion_id"   gorm:"column:suggestion_id;primaryKey;not null"`
-	SuggestedBy 	 string 	 `json:"suggested_by"    gorm:"column:suggested_by;not null"`
-	SuggestionText string 	 `json:"suggestion_text" gorm:"column:suggestion_text;not null"`
-	SuggestionDTM  time.Time `json:"suggestion_dtm"  gorm:"column:suggestion_dtm;not null"`
+	ID		int		`json:"suggestion_id"   gorm:"column:suggestion_id;primaryKey;not null"`
+	SuggestedBy 	string 	 	`json:"suggested_by"    gorm:"column:suggested_by;not null"`
+	SuggestionText 	string 	 	`json:"suggestion_text" gorm:"column:suggestion_text;not null"`
+	SuggestionDTM  	time.Time 	`json:"suggestion_dtm"  gorm:"column:suggestion_dtm;not null"`
 }
 
 func (Suggestion) TableName() string {
