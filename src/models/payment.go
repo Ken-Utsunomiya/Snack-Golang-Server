0dc39a5cd1fd4b13d70ff8289ba2ef84359a0fcf
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-02 16:50:24 -0700

    Update models

--- src/models/payment.go
+++ src/models/payment.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Payment struct {
-	ID						int				`json:"payment_id"			gorm:"column:payment_id;primaryKey;autoIncrement;not null"`
-	UserID				int				`json:"user_id"					gorm:"column:user_id;not null"`
-	PaymentAmount	int				`json:"payment_amount"	gorm:"column:payment_amount;not null"`
-	PaymentDTM		time.Time	`json:"payment_dtm"			gorm:"column:payment_dtm;not null"`
-	CreatedBy			string		`json:"created_by"			gorm:"column:created_by;not null;size:128"`
-	User					User
+	ID		int		`json:"payment_id"     gorm:"column:payment_id;primaryKey;autoIncrement;not null"`
+	UserID        	int       	`json:"user_id"        gorm:"column:user_id;not null"`
+	PaymentAmount 	int 		`json:"payment_amount" gorm:"column:payment_amount;not null"`
+	PaymentDTM 	time.Time 	`json:"payment_dtm"    gorm:"column:payment_dtm;not null"`
+	CreatedBy 	string 		`json:"created_by"     gorm:"column:created_by;not null;size:128"`
+	User          	User
 }
 
 func (Payment) TableName() string {
