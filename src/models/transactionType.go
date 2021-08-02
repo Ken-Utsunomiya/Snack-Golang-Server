e932e46cf8a60f58619dea0dcc36c3f20576f2ff
Ken-Utsunomiya <81115999+Ken-Utsunomiya@users.noreply.github.com>
2021-08-02 16:25:16 -0700

    Update transactionType.go

--- src/models/transactionType.go
+++ src/models/transactionType.go
@@ -1,8 +1,8 @@
 package models
 
 type TransactionType struct {
-	ID 	 int 	  `json:"transaction_type_id"   gorm:"column:transaction_type_id;primaryKey;not null"`
-	Name string `json:"transaction_type_name" gorm:"column:transaction_type_name;unique;not null;size:128"`
+	ID	int	`json:"transaction_type_id"   gorm:"column:transaction_type_id;primaryKey;not null"`
+	Name 	string 	`json:"transaction_type_name" gorm:"column:transaction_type_name;unique;not null;size:128"`
 }
 
 func (TransactionType) TableName() string {
