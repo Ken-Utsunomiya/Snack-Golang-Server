ebd34fd122e2785638b53079ba997eb290ca8de7
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-04 04:15:28 -0700

    fix snackbatch model

--- src/models/snackbatch.go
+++ src/models/snackbatch.go
@@ -6,9 +6,10 @@ import (
 
 type SnackBatch struct {
 	ID 		int		`json:"snack_batch_id" gorm:"column:snack_batch_id;primaryKey;not null"`
-	SnackID       	int          	`json:"snack_id"       gorm:"column:snack_id;not null"`
+	SnackID       	int          	`json:"snack_id"       gorm:"column:snack_id;not null;TYPE:integer REFERENCES snacks"`
 	Quantity 	int 		`json:"quantity"       gorm:"column:quantity;not null"`
 	ExpirationDTM 	sql.NullTime 	`json:"expiration_dtm" gorm:"column:expiration_dtm"`
+	Snack		Snack
 }
 
 func (SnackBatch) TableName() string {
