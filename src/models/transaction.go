959a3b2943a562933b976b761d626d6466a8e2d6
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-04 04:15:05 -0700

    fix transaction model

--- src/models/transaction.go
+++ src/models/transaction.go
@@ -6,9 +6,9 @@ import (
 
 type Transaction struct {
 	ID			int 		`json:"transaction_id"      gorm:"column:transaction_id;primaryKey;not null"`
-	UserID            	int       	`json:"user_id"             gorm:"column:user_id;not null"`
-	PaymentID         	int       	`json:"payment_id"          gorm:"column:payment_id;not null"`
-	TransactionTypeID 	int       	`json:"transaction_type_id" gorm:"column:transaction_type_id;not null"`
+	UserID            	int       	`json:"user_id"             gorm:"column:user_id;not null;TYPE:integer REFERENCES users"`
+	PaymentID         	int       	`json:"payment_id"          gorm:"column:payment_id;not null;TYPE:integer REFERENCES payments"`
+	TransactionTypeID 	int       	`json:"transaction_type_id" gorm:"column:transaction_type_id;not null;TYPE:integer REFERENCES transaction_types"`
 	SnackName 		string 		`json:"snack_name"          gorm:"column:snack_name;not null;size:128"`
 	TransactionAmount 	int 		`json:"transaction_amount"  gorm:"column:transaction_amount;not null"`
 	Quantity 		int 		`json:"quantity"            gorm:"column:quantity;not null"`
