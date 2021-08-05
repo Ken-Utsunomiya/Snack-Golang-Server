84d709935a636ad067aa50624aef76ce4b8f33c3
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-05 14:27:57 -0700

    feat: implement get user payments

--- src/handlers/payment.go
+++ src/handlers/payment.go
@@ -1,9 +1,35 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"Snack-Golang-Server/src/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
 func UserPaymentList(c *gin.Context) {
+	userService := services.UserService{}
+	paymentService := services.PaymentService{}
 
+	userId, err := strconv.Atoi(c.Param("user_id"))
+	if err != nil {
+		c.Error(err).SetType(gin.ErrorTypePublic)
+		return
+	}
+
+	_, err = userService.GetUser(userId)
+	if err != nil {
+		c.Error(err).SetType(gin.ErrorTypePublic)
+		return
+	}
+
+	userPaymentList, err := paymentService.GetUserPaymentList(userId)
+	if err != nil {
+		c.Error(err).SetType(gin.ErrorTypePublic)
+		return
+	}
+
+	c.JSON(http.StatusOK, userPaymentList)
 }
 
 func PaymentCreate(c *gin.Context) {
--- src/services/payment.go
+++ src/services/payment.go
@@ -1,11 +1,17 @@
 package services
 
-import "Snack-Golang-Server/src/models"
+import (
+	"Snack-Golang-Server/src/database"
+	"Snack-Golang-Server/src/models"
+)
 
 type PaymentService struct {}
 
-func (PaymentService) GetUserPaymentList() []models.Payment {
-	return nil
+func (PaymentService) GetUserPaymentList(userId int) ([]models.Payment, error) {
+	db := database.GetDB()
+	paymentList := make([]models.Payment, 0)
+	err := db.Order("payment_dtm desc").Find(&paymentList, models.Payment{ UserID: userId }).Error
+	return paymentList, err
 }
 
 func (PaymentService) AddPayment(payment *models.Payment) error {
