8fdf29a8d9658fa1773051050da23a5bd58cc79f
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-07 02:04:34 -0700

    fix: fix pagination response model

--- src/utils/pagination.go
+++ src/utils/pagination.go
@@ -14,7 +14,6 @@ type Pagination struct {
 	Limit        int         `json:"limit,omitempty;query:limit"`
 	Page         int         `json:"page,omitempty;query:page"`
 	Sort         string      `json:"sort,omitempty;query:sort"`
-	Rows         interface{} `json:"rows"`
 }
 
 type PaginationResponse struct {
