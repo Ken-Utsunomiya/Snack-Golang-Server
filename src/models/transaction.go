302ad0c5994d0b53fc32b3b2d11874a581cf2495
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-12 14:16:34 -0700

    fix: remove gorm tags from transaction model

--- src/models/transaction.go
+++ src/models/transaction.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Transaction struct {
-	ID			int 		`json:"transaction_id"      gorm:"column:transaction_id;primaryKey;not null"`
-	UserID            	int     	`json:"user_id"             gorm:"column:user_id;not null;"`
-	PaymentID         	int     	`json:"payment_id"          gorm:"column:payment_id;not null;"`
-	TransactionTypeID 	int     	`json:"transaction_type_id" gorm:"column:transaction_type_id;not null;"`
-	SnackName 		string 		`json:"snack_name"          gorm:"column:snack_name;not null;size:128"`
-	TransactionAmount 	int 		`json:"transaction_amount"  gorm:"column:transaction_amount;not null"`
-	Quantity 		int 		`json:"quantity"            gorm:"column:quantity;not null"`
-	TransactionDTM 		time.Time 	`json:"transaction_dtm"     gorm:"column:transaction_dtm;not null"`
+	ID			int 		`json:"transaction_id"`
+	UserID            	int     	`json:"user_id"`
+	PaymentID         	int     	`json:"payment_id"`
+	TransactionTypeID 	int     	`json:"transaction_type_id"`
+	SnackName 		string 		`json:"snack_name"`
+	TransactionAmount 	int 		`json:"transaction_amount"`
+	Quantity 		int 		`json:"quantity"`
+	TransactionDTM 		time.Time 	`json:"transaction_dtm"`
 }
 
 func (Transaction) TableName() string {
