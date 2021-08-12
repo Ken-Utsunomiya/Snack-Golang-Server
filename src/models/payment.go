ca3996e02c6d7a14753c74adaa39be7423c856fc
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-12 14:18:52 -0700

    fix: remove gorm tags from payment model

--- src/models/payment.go
+++ src/models/payment.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Payment struct {
-	ID		int		`json:"payment_id"     gorm:"column:payment_id;primaryKey;autoIncrement;not null"`
-	UserID        	int       	`json:"user_id"        gorm:"column:user_id;not null;"`
-	PaymentAmount 	int 		`json:"payment_amount" gorm:"column:payment_amount;not null"`
-	PaymentDTM 	time.Time 	`json:"payment_dtm"    gorm:"column:payment_dtm;not null"`
-	CreatedBy 	string 		`json:"created_by"     gorm:"column:created_by;not null;size:128"`
+	ID		int		`json:"payment_id"`
+	UserID        	int       	`json:"user_id"`
+	PaymentAmount 	int 		`json:"payment_amount"`
+	PaymentDTM 	time.Time 	`json:"payment_dtm"`
+	CreatedBy 	string 		`json:"created_by"`
 }
 
 func (Payment) TableName() string {
