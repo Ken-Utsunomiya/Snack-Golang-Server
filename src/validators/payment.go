2f9d2a6a68ca303e8016af4362732c14a6dfeedb
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-14 18:45:12 -0700

    feat: add func RegisterRequestToPaymentModel

--- src/validators/payment.go
+++ src/validators/payment.go
@@ -1,8 +1,22 @@
 package validators
 
+import (
+	"Snack-Golang-Server/src/models"
+	"time"
+)
+
 type PaymentRegisterRequest struct {
 	UserID						int `json:"user_id" binding:"required"`
 	CreatedBy	string `json:"created_by" binding:"required"`
 	PaymentAmount	int `json:"payment_amount" binding:"required"`
 	TransactionIDs	[]int `json:"transaction_ids" binding:"required"`
+}
+
+func RegisterRequestToPaymentModel(request PaymentRegisterRequest) models.Payment {
+	payment := models.Payment{}
+	payment.UserID = request.UserID
+	payment.CreatedBy = request.CreatedBy
+	payment.PaymentAmount = request.PaymentAmount
+	payment.PaymentDTM = time.Now()
+	return payment
 }
