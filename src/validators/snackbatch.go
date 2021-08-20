e01baab1b55a92bf88e27f283e335fdf7519c52a
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-20 14:34:40 -0700

    feat: add SnackBatchUpdateRequest

--- src/validators/snackbatch.go
+++ src/validators/snackbatch.go
@@ -2,12 +2,18 @@ package validators
 
 import (
 	"Snack-Golang-Server/src/models"
+	"time"
 )
 
 type SnackBatchRegisterRequest struct {
 	SnackID	int `json:"snack_id" binding:"required"`
 	Quantity	int `json:"quantity" binding:"required,min=1"`
 	ExpirationDTM	string `json:"expiration_dtm" binding:"required"`
+}
+
+type SnackBatchUpdateRequest struct {
+	Quantity	int	`json:"quantity" binding:"required"`
+	ExpirationDTM	*time.Time	`json:"expiration_dtm"`
 }
 
 func RegisterRequestToSnackBatchModel(request SnackBatchRegisterRequest) models.SnackBatch {
