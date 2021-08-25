4b67e6aa453817d90a5a93021a39f97c5212479d
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-25 16:32:11 -0700

    feat: implement snack create handler

--- src/handlers/snack.go
+++ src/handlers/snack.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"Snack-Golang-Server/src/services"
+	"Snack-Golang-Server/src/validators"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -20,7 +21,21 @@ func SnackList(c *gin.Context) {
 }
 
 func SnackCreate(c *gin.Context) {
+	snackService := services.SnackService{}
 
+	snackRegisterRequest := validators.SnackRegisterRequest{}
+	if err := c.ShouldBindJSON(&snackRegisterRequest); err != nil {
+		_ = c.Error(err).SetType(gin.ErrorTypePublic)
+		return
+	}
+
+	res, err := snackService.AddSnack(snackRegisterRequest)
+	if err != nil {
+		_ = c.Error(err).SetType(gin.ErrorTypePublic)
+		return
+	}
+
+	c.JSON(http.StatusCreated, res)
 }
 
 func SnackUpdate(c *gin.Context) {
