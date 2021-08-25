b04151ba0e561a08f9ff609024984bbc5c40f0fd
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-08-25 16:28:01 -0700

    feat: add RegisterRequestToSnackModel func

--- src/validators/snack.go
+++ src/validators/snack.go
@@ -1,6 +1,8 @@
 package validators
 
-import "time"
+import (
+	"Snack-Golang-Server/src/models"
+)
 
 type SnackRegisterRequest struct {
 	SnackName	string	`json:"snack_name" binding:"required"`
@@ -10,6 +12,21 @@ type SnackRegisterRequest struct {
 	Price	int	`json:"price" binding:"required"`
 	IsActive	bool	`json:"is_active" binding:"required"`
 	OrderThreshold	*int	`json:"order_threshold"`
-	LastUpdatedDTM	time.Time	`json:"last_updated_dtm" binding:"required"`
 	LastUpdatedBy	string	`json:"last_updated_by" binding:"required"`
+}
+
+func RegisterRequestToSnackModel(request SnackRegisterRequest) models.Snack {
+	snack := models.Snack{}
+	snack.Name = request.SnackName
+	snack.SnackTypeID = request.SnackTypeID
+	snack.Description = request.Description
+	snack.ImageURI = request.ImageURI
+	snack.Price = request.Price
+	snack.IsActive = request.IsActive
+	snack.LastUpdatedBy = request.LastUpdatedBy
+	snack.OrderThreshold = request.OrderThreshold
+	if request.OrderThreshold != nil {
+		snack.OrderThreshold = request.OrderThreshold
+	}
+	return snack
 }
